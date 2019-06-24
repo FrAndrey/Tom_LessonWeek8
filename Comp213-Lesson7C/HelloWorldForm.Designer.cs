08b129011eec92b447f3953aa89fbac9725167cc
aparamon <301038409@cencol.ca>
2019-06-24 16:14:29 -0400

    Add project files.

---
+++ Comp213-Lesson7C/HelloWorldForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Comp213_Lesson7C
+{
+    partial class HelloWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloWorldLabel = new System.Windows.Forms.Label();
+            this.ClickMeButton = new System.Windows.Forms.Button();
+            this.AnotherOneLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HelloWorldLabel
+            // 
+            this.HelloWorldLabel.AutoSize = true;
+            this.HelloWorldLabel.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.HelloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelloWorldLabel.Location = new System.Drawing.Point(300, 250);
+            this.HelloWorldLabel.Name = "HelloWorldLabel";
+            this.HelloWorldLabel.Size = new System.Drawing.Size(276, 54);
+            this.HelloWorldLabel.TabIndex = 0;
+            this.HelloWorldLabel.Text = "Hello World!";
+            this.HelloWorldLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClickMeButton
+            // 
+            this.ClickMeButton.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClickMeButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClickMeButton.Location = new System.Drawing.Point(388, 345);
+            this.ClickMeButton.Name = "ClickMeButton";
+            this.ClickMeButton.Size = new System.Drawing.Size(106, 44);
+            this.ClickMeButton.TabIndex = 1;
+            this.ClickMeButton.Text = "Click me!";
+            this.ClickMeButton.UseVisualStyleBackColor = false;
+            this.ClickMeButton.Click += new System.EventHandler(this.ClickMeButton_Click);
+            // 
+            // AnotherOneLabel
+            // 
+            this.AnotherOneLabel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.AnotherOneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AnotherOneLabel.Location = new System.Drawing.Point(278, 82);
+            this.AnotherOneLabel.Name = "AnotherOneLabel";
+            this.AnotherOneLabel.Size = new System.Drawing.Size(196, 92);
+            this.AnotherOneLabel.TabIndex = 2;
+            this.AnotherOneLabel.Text = "Another Label";
+            this.AnotherOneLabel.Click += new System.EventHandler(this.AnotherOneLabel_Click);
+            // 
+            // HelloWorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(844, 601);
+            this.Controls.Add(this.AnotherOneLabel);
+            this.Controls.Add(this.ClickMeButton);
+            this.Controls.Add(this.HelloWorldLabel);
+            this.ForeColor = System.Drawing.Color.AliceBlue;
+            this.Name = "HelloWorldForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloWorldLabel;
+        private System.Windows.Forms.Button ClickMeButton;
+        private System.Windows.Forms.Label AnotherOneLabel;
+    }
+}
+
---
+++ Comp213-Lesson7C/HelloWorldForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comp213_Lesson7C
+{
+    public partial class HelloWorldForm : Form
+    {
+        /// <summary>
+        /// This is the constuctor for the HelloWorld Form
+        /// </summary>
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+            Start();
+        }
+        /// <summary>
+        /// This method initializes Form and Control properties
+        /// </summary>
+        public void Start()
+        {
+            HelloWorldLabel.Text = "Hello, Andrey!";
+            HelloWorldLabel.BackColor = Color.Yellow;
+            HelloWorldLabel.ForeColor = Color.Black;
+      
+
+
+
+        }
+
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            HelloWorldLabel.Text = (HelloWorldLabel.Text == "Clicked") ? "Unclicked" : "Clicked";
+            generateRandomNumber();
+          
+        }
+
+
+        private void generateRandomNumber()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 99);
+            AnotherOneLabel.Text = (AnotherOneLabel.Text == "Clicked") ? $"{number}" : $"{number}";
+
+            //OR
+            // Random rand = new Random();
+            //int number = rand.Next(1, 99);
+            //AnotheOneLabel.Text = number.ToString();
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+        private void AnotherOneLabel_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
